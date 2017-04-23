b858c02a28d11f0a227e3365cd6cf9469594ec3c
WIN-CGHV9IU3CDE\Administrator <sy8095@gmail.com>
2017-04-24 00:38:44 +0900

    change design

---
+++ static/js/index.js
@@ -0,0 +1,61 @@
+$(function(){
+
+    var slide_width = $('body').outerWidth();
+
+    $('.slide_container').css('width', slide_width * 4);
+    $('.slide_container > div[class*="slide_"]').css({
+        'width' : slide_width
+    });
+
+    movingSlide(0);
+
+    $('.header .navigation li').on('click', function(){
+        $('.header .navigation .clicked').removeClass('clicked');
+        $(this).addClass('clicked');
+    });
+
+    $('.career_container .btn_slide > button').on('click', function(){
+        if ($(this).hasClass('disabled')) {
+            return;
+        } else if ($(this).hasClass('animate')) {
+            return;
+        } else {
+            $(this).addClass('animate');
+            var $container = $(this).parents('.container_1').find('.slide_all');
+            var offset = parseInt($container.css('left'));
+            $container.animate({
+                'left' : $(this).hasClass('btn_next')? (offset -= 640) : (offset += 640)
+            }, 200, function(){
+                $('button.animate').removeClass('animate');
+            });
+            if (offset == 0){
+                $('.btn_prev').addClass('disabled');
+            } else if (offset == -1920) {
+                $('.btn_next').addClass('disabled');
+            } else {
+                $('.btn_prev').removeClass('disabled');
+                $('.btn_next').removeClass('disabled');
+            }
+        }
+    });
+
+});
+
+function radioDot(id){
+    $('.dot_container').find('.fa-circle').attr('class', 'fa fa-circle-o');
+    $('.dot_container'+' .'+id).find('.fa-circle-o').attr('class', 'fa fa-circle');
+}
+
+function movingSlide(count){
+    setInterval(function(){
+        var div_arr = $('.slide_container div[class*="slide"]');
+        $('.slide_container div[class*="slide"]').css('display', 'none');
+        $('.slide_container div.showed').removeClass('showed');
+        var showed_div = div_arr[count++ % div_arr.length];
+        showed_div.style.display = 'block';
+        showed_div.classList.add('showed');
+
+        var showed_id = $('.slide_container .showed').attr('id');
+        radioDot(showed_id);
+    }, 2000);
+}
